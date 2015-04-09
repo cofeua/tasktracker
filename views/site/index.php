182bb47a6e6909f521932dc2b3bfdb55d3915584
Viktor <cofeua@gmail.com>
2015-04-10 01:46:56 +0300

    Init project

---
+++ views/site/index.php
@@ -0,0 +1,12 @@
+<?php
+/* @var $this yii\web\View */
+$this->title = '';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>тут ничего нет!</h1>
+    </div>
+
+    </div>
+</div>
